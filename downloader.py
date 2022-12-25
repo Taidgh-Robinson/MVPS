20e0d6bbb5a282d6f743dae9760f3e594225ad1a
Taidgh Robinson <taidgh.robinson@maine.edu>
2022-12-25 15:49:24 -0800

    add downloaded data + more methods

--- downloader.py
+++ downloader.py
@@ -15,7 +15,6 @@ def gen_ids():
     pdf.to_csv('data/ids.csv')
 
 
-#gen_ids()
 
 def download_player_career_stats():
     df = pd.read_csv('data/ids.csv')
@@ -26,4 +25,15 @@ def download_player_career_stats():
         pdf = career.get_data_frames()[0]
         pdf.to_csv('data/player_career_stats/{}.csv'.format(str(id)))
 
-download_player_career_stats()
+#gen_ids()
+#download_player_career_stats()
+
+df = pd.read_csv('mvps.csv')
+df1 = pd.read_csv('data/ids.csv')
+for index, row in df.iterrows():
+    print(row['Season'] + ':' +row['Player'])
+    id = int((df1.loc[df1['name'] == row['Player']])['id'])
+    print(id)
+    data = pd.read_csv('data/player_career_stats/{}.csv'.format(str(id)))
+    rrrr = data.loc[data['SEASON_ID'] == row['Season']]
+    print(rrrr)
